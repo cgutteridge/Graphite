00d07c84825d355e7a1616981eca1c9dc3e2abd9
Bart Nagel <bart@tremby.net>
2011-03-01 16:17:55 +0000

    documented isNull()

--- graphite/Graphite.php
+++ graphite/Graphite.php
@@ -22,8 +22,7 @@
 
 # to document:
 # added sioc: (needs to be documented in the parent of graphite-docs.html),
-# addTriples, addTriple, isNull,
-# loadSPARQL
+# addTriples, addTriple, loadSPARQL
 
 
 class Graphite
