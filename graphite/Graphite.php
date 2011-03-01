ce2e962db8add3e4bd87c0a1dbc994bf4ab1a07f
Bart Nagel <bart@tremby.net>
2011-03-01 16:10:51 +0000

    documented isType()

--- graphite/Graphite.php
+++ graphite/Graphite.php
@@ -22,7 +22,7 @@
 
 # to document:
 # added sioc: (needs to be documented in the parent of graphite-docs.html),
-# addTriples, addTriple, isType, allSubjects, allObjects, isNull,
+# addTriples, addTriple, allSubjects, allObjects, isNull,
 # loadSPARQL
 
 
