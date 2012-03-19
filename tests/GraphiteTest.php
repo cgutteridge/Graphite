72b8e8914fc48e352dd170f3adc81a05f7177d90
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-03-19 23:04:34 +1030

    Further coverage of addTriples()

--- tests/GraphiteTest.php
+++ tests/GraphiteTest.php
@@ -39,6 +39,8 @@ class Graphite_Test extends PHPUnit_Framework_TestCase {
         $this->assertSame("* This Document *", $this->graph->shrinkURI(null));
 
         $this->assertSame('foaf:knows', $this->graph->shrinkURI('http://xmlns.com/foaf/0.1/knows'));
+
+        $this->assertSame('http://xmlns.com/foaf/0.2/knows', $this->graph->shrinkURI('http://xmlns.com/foaf/0.2/knows'));
     }
 
     /**
@@ -72,6 +74,94 @@ class Graphite_Test extends PHPUnit_Framework_TestCase {
         $this->assertSame('file:///odd/uri/scheme', $this->graph->cleanURI('file:///odd/uri/scheme'));
     }
 
+    public function testAllSubjectsWithEmptyGraph() {
+        $list = $this->graph->allSubjects();
+
+        $this->assertTrue($list instanceof Graphite_ResourceList);
+        $this->assertSame(0, count($list));
+    }
+
+
+    public function testAddTriple() {
+        $this->graph->addTriple('http://my.com/cat#', "http://smells.com/nose_quanity", "Zero");
+
+        // Subject predicate object relation stored?
+        $this->assertTrue(array_key_exists(
+            'http://my.com/cat#',
+            $this->graph->t["sp"]
+        ));
+
+        $this->assertTrue(array_key_exists(
+            'http://smells.com/nose_quanity',
+            $this->graph->t["sp"]['http://my.com/cat#']
+        ));
+
+        $this->assertSame(
+            array(0 => "Zero"),           
+            $this->graph->t["sp"]['http://my.com/cat#']['http://smells.com/nose_quanity']
+        );
+
+        // object predicate subject relation stored?
+        // TODO: Think about this or literal values of int(0) which are type cast to string ''
+        $this->assertTrue(array_key_exists(
+            'Zero',
+            $this->graph->t["op"]
+        ));
+
+        $this->assertTrue(array_key_exists(
+            'http://smells.com/nose_quanity',
+            $this->graph->t["op"]['Zero']
+        ));
+
+        $this->assertSame(
+            array(0 => "http://my.com/cat#"),           
+            $this->graph->t["op"]['Zero']['http://smells.com/nose_quanity']
+        );
+
+    }
+
+    public function testAllSubjectsWithTriples() {
+        $this->graph->addTriple('http://my.com/cat#', "http://smells.com/nose_quanity", "Zero");
+        $this->graph->addTriple('http://you.com/#', "http://question.com/question", "http://question.com/1#");
+        $this->graph->addTriple('http://question.com/1#', "dc:title", "How does he smell?");
+        $this->graph->addTriple('http://my.com/cat#', "http://smells.com/smells", "Terribly");
+
+        $list = $this->graph->allSubjects();
+
+        $this->assertTrue($list instanceof Graphite_ResourceList);
+        $this->assertSame(3, count($list));
+
+        $this->assertTrue($list[0] instanceof Graphite_Resource);
+        $this->assertTrue($list[1] instanceof Graphite_Resource);
+        $this->assertTrue($list[2] instanceof Graphite_Resource);
+
+        $this->assertSame("http://my.com/cat#", $list[0]->uri);
+        $this->assertSame("http://you.com/#", $list[1]->uri);
+        $this->assertSame("http://question.com/1#", $list[2]->uri);
+    }
+
+    public function testAllObjectsWithTriples() {
+        $this->graph->addTriple('http://my.com/cat#', "http://smells.com/nose_quanity", "Zero");
+        $this->graph->addTriple('http://you.com/#', "http://question.com/question", "http://question.com/1#");
+        $this->graph->addTriple('http://question.com/1#', "dc:title", "How does he smell?");
+        $this->graph->addTriple('http://my.com/cat#', "http://smells.com/smells", "Terribly");
+
+        $list = $this->graph->allObjects();
+
+        $this->assertTrue($list instanceof Graphite_ResourceList);
+        $this->assertSame(4, count($list));
+
+        $this->assertTrue($list[0] instanceof Graphite_Resource);
+        $this->assertTrue($list[1] instanceof Graphite_Resource);
+        $this->assertTrue($list[2] instanceof Graphite_Resource);
+        $this->assertTrue($list[3] instanceof Graphite_Resource);
+
+        $this->assertSame("Zero", $list[0]->uri);
+        $this->assertSame("http://question.com/1#", $list[1]->uri);
+        $this->assertSame("How does he smell?", $list[2]->uri);
+        $this->assertSame("Terribly", $list[3]->uri);
+    }
+
     public function test() {
         $this->markTestIncomplete('
 	public function __construct( $namespaces = array(), $uri = null )
@@ -96,7 +186,6 @@ class Graphite_Test extends PHPUnit_Framework_TestCase {
 	public function serialize( $type = "RDFXML" )
 	public function primaryTopic( $uri = null )
 	public function allOfType( $uri )
-	public function allSubjects()
 	public function allObjects()
 	public function dump( $options=array() )
 	public function dumpText( $options=array() )
