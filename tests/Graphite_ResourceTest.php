f9adfb6a21c2ad71fec04077d01cf4da807d86e3
Daniel O'Connor <daniel.oconnor@rpdata.com>
2012-03-20 13:23:17 +1030

    Add more tests

--- tests/Graphite_ResourceTest.php
+++ tests/Graphite_ResourceTest.php
@@ -11,7 +11,7 @@ class Graphite_ResourceTest extends PHPUnit_Framework_TestCase {
 	public function get( /* List */ )
 
 	public function getLiteral( /* List */ )
-	# getString deprecated in favour of getLiteral 
+	# getString deprecated in favour of getLiteral
 	public function getString( /* List */ ) { return $this->getLiteral( func_get_args() ); }
 
 	public function getDatatype( /* List */ )
@@ -42,5 +42,129 @@ class Graphite_ResourceTest extends PHPUnit_Framework_TestCase {
 	function nodeType() { return "#resource"; }
 
 	function prepareDescription()');
+    }
+
+    public function setUp() {
+        $this->resource = new Graphite_Resource(new Graphite(), null);
+    }
+
+    public function testPrepareDescription() {
+        $description = $this->resource->prepareDescription();
+
+        $this->assertTrue($description instanceof Graphite_Description);
+    }
+
+    public function testDumpValueText() {
+        $this->assertSame("", $this->resource->dumpValueText());
+    }
+
+    public function testDumpValue() {
+        $this->assertSame("<a href='' title='' style='text-decoration:none;color:red'></a>", $this->resource->dumpValue());
+
+        $this->resource->uri = 'http://whee.com';
+
+        $this->assertSame("<a href='http://whee.com' title='http://whee.com' style='text-decoration:none;color:red'></a>", $this->resource->dumpValue());
+
+        $this->assertSame("<a href='#http://whee.com' title='http://whee.com' style='text-decoration:none;color:red'></a>", $this->resource->dumpValue(array('internallinks' => true)));
+    }
+
+    public function testNodeType() {
+        $this->assertSame("#resource", $this->resource->nodeType());
+    }
+
+
+    public function test__toString() {
+        $this->assertSame("", (string)$this->resource);
+    }
+
+    public function testDump() {
+        $this->assertSame("", $this->resource->dump());
+        $this->assertSame("", $this->resource->dump(array('label' => true)));
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+    public function testDumpText() {
+        $this->assertSame("", $this->resource->dumpText());
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+    public function testPrettyLink() {
+        $this->assertSame("<a title='' href=''></a>", $this->resource->prettyLink());
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+
+    public function testLink() {
+        $this->assertSame(null, $this->resource->link());
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+    public function testLabel() {
+        $this->assertSame(null, $this->resource->label());
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+
+    public function testIsType() {
+        $this->assertFalse($this->resource->isType());
+        $this->assertFalse($this->resource->isType(null));
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+
+    public function testType() {
+        $type = $this->resource->type();
+        $this->assertTrue($type instanceof Graphite_Null);
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+    public function testTypes() {
+        $this->assertSame("", $this->resource->types());
+
+        $this->markTestIncomplete("Needs further coverage");
+    }
+
+
+    public function testAll() {
+        $all = $this->resource->all();
+
+        $this->assertTrue($all instanceof Graphite_ResourceList);
+        $this->assertSame(0, count($all));
+    }
+
+
+    public function testLoad() {
+        $this->assertSame(0, $this->resource->load());
+    }
+
+    public function testLoadSameAs() {
+        $this->assertSame(0, $this->resource->loadSameAs(null));
+    }
+
+    public function testLoadSameAsOrg() {
+        $this->assertSame(0, $this->resource->loadSameAsOrg(null));
+    }
+
+    public function testLoadDataGovUKBackLinks() {
+        $this->assertSame(0, $this->resource->loadDataGovUKBackLinks());
+    }
+
+    public function testToArcTriples() {
+        $this->assertSame(array(), $this->resource->toArcTriples());
+
+        $this->resource->g->addTriple(array('s' => 'http://my.com/dog#', 'p' => '#smells', 'o' => 'http://wikipedioa.org/Terribly#'));
+
+        $this->uri = 'http://my.com/dog#';
+        $this->assertSame(array(), $this->resource->toArcTriples());
+
+
+        $this->assertSame(array(), $this->resource->toArcTriples(true));
     }
 }
