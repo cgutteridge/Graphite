cgutteridge/Graphite

commit 2b48f4fcb03cbb493f36d300e0686148ee2507c4

Fix invalid __toString behaviour

Graphite/Literal.php
@@ -8,7 +8,9 @@ class Graphite_Literal extends Graphite_Node
 		$this->v = $triple["v"];
 	}
 
-	function __toString() { return $this->triple["v"]; }
+	function __toString() { 
+        return !empty($this->triple["v"]) ? (string)$this->triple['v'] : ""; 
+    }
 	function datatype() { return @$this->triple["d"]; }
 	function language() { return @$this->triple["l"]; }
 

commit e655f1555cc109e349f1dcbf150e27f1f513b3e3

Fix regression caused by 2b48f4fcb03cbb493f36d300e0686148ee2507c4

Graphite/Literal.php
@@ -18,7 +18,7 @@ class Graphite_Literal extends Graphite_Node
 	}
 
 	function __toString() {
-		return !empty($this->triple["v"]) ? (string)$this->triple['v'] : "";
+		return isset($this->triple["v"]) ? (string)$this->triple['v'] : "";
 	}
 	function datatype() { return @$this->triple["d"]; }
 	function language() { return @$this->triple["l"]; }